79cc9b8e90ff6b5304de261359eb66afa3359209
jgomez04 <juligo0404@gmail.com>
2024-12-20 12:38:58 -0500

    Add: add div table

--- test-react-ts/src/App.tsx
+++ test-react-ts/src/App.tsx
@@ -12,18 +12,21 @@ function App() {
 
   
       <div className='h-screen mt-1'>
-      <div className="relative p-6 rounded-lg h-[700px] half-bg pl-[100px]">
-        <div className='flex-col mt-20'>
-          <h2 className="relative text-7xl font-bold text-gray-800 mt-12 w-[900px] font-serif">Surprise your favorite person</h2>
-          <p className="relative text-gray-600 mt-6"><span className='font-extrabold text-pink-500 italic'>New</span> Rose arrangement with golden vase</p>
-          <h3 className="relative mt-6 text-4xl font-extrabold text-pink-500 italic">$99.00</h3>
-          <button className="relative mt-6 bg-[#ffaa01] hover:bg-[#ffaa01]-600 text-black font-bold py-2 px-4 rounded-full w-[240px]">Shop now</button>
+        <div className="relative p-6 rounded-lg h-[700px] half-bg pl-[100px]">
+          <div className='flex-col mt-20'>
+            <h2 className="relative text-7xl font-bold text-gray-800 mt-12 w-[900px] font-serif">Surprise your favorite person</h2>
+            <p className="relative text-gray-600 mt-6"><span className='font-extrabold text-pink-500 italic'>New</span> Rose arrangement with golden vase</p>
+            <h3 className="relative mt-6 text-4xl font-extrabold text-pink-500 italic">$99.00</h3>
+            <button className="relative mt-6 bg-[#ffaa01] hover:bg-[#ffaa01]-600 text-black font-bold py-2 px-4 rounded-full w-[240px]">Shop now</button>
+          </div>
+          
+          <div className="absolute top-[100px] right-20 z-10">
+            <img src={Grupo5} alt="icon" className='h-[500px] w-[500px]'/>
+          </div>
+
+          <div className='absolute bottom-0 right-0 rounded-tl-[90px] rounded-bl-[90px] bg-[#f6dcdc] text-white p-4 h-[178px] w-[778px] z-0'>
+          </div>
         </div>
-        
-        <div className="absolute top-10 right-20">
-          <img src={Grupo5} alt="icon" className='h-[500px] w-[500px]'/>
-        </div>
-      </div>
       </div>
       
 
